cd739634ea54a32495362eb32493b5c4508b4ae2
Dan Laugharn <laugharn@gmail.com>
2018-08-29 20:23:27 -0500

    ♻️ Adds to Nuxt config

--- packages/driving/nuxt.config.js
+++ packages/driving/nuxt.config.js
@@ -1,4 +1,9 @@
 import path from "path";
+import pkg from "./package.json";
+
+const config = dotenv.config({
+    path: `./.env.${process.env.ENV_FILE || "local"}`
+}).parsed;
 
 const corePath =
     process.env.NODE_ENV == "production"
@@ -15,10 +20,52 @@ export default {
         extend(config) {
             config.resolve.alias["@core"] = corePath;
             config.resolve.alias["@modules"] = modulesDirPath;
+        },
+
+        optimization: {
+            splitChunks: {
+                name: true
+            }
         }
     },
 
     env: {
-        ENV_FILE: process.env.ENV_FILE
+        BUILD_NUM: pkg.version,
+        ENV_FILE: process.env.ENV_FILE,
+        ...config
+    },
+
+    icon: {
+        iconSrc: corePath + "/assets/img/icon.png"
+    },
+
+    modules: [
+        modulesDirPath + "/@nuxtjs/icon",
+        modulesDirPath + "/@nuxtjs/meta"
+    ],
+
+    plugins: [
+        {
+            src: corePath + "/plugins/directives.js",
+            ssr: false
+        },
+        {
+            src: corePath + "/plugins/gtm",
+            ssr: false
+        },
+        {
+            src: corePath + "/plugins/storage",
+            ssr: false
+        }
+    ],
+
+    render: {
+        gzip: {
+            threshold: 5
+        },
+        http2: {
+            push: true
+        },
+        maxAge: 360000
     }
 };
