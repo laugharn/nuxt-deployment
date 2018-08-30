ef202546c7d71a7a584cb61cfec3a92288bdf05d
Dan Laugharn <laugharn@gmail.com>
2018-08-29 20:01:29 -0500

    🐛 Fixes index page

--- packages/driving/pages/index.vue
+++ packages/driving/pages/index.vue
@@ -1,21 +1,3 @@
-<script>
-import hello from "@core/assets/js/hello";
-
-export default {
-    computed: {
-        hello() {
-            return hello(this.name) + ", this is on " + process.env.ENV_FILE;
-        }
-    },
-
-    data() {
-        return {
-            name: "Kevin"
-        };
-    }
-};
-</script>
-
 <template>
-    <div v-html="hello"></div>
+    <div>Hello</div>
 </template>
