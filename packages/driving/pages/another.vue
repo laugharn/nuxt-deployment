76dfcad149060198ce0d35df24bb27797fc1566d
Dan Laugharn <laugharn@gmail.com>
2018-08-30 14:37:40 -0500

    ✨ Adds another page

---
+++ packages/driving/pages/another.vue
@@ -0,0 +1,26 @@
+<script>
+import ultra from "@core/assets/js/services/ultra/query";
+
+export default {
+    data() {
+        return {
+            products: null
+        };
+    },
+
+    async mounted() {
+        this.products = await ultra({
+            post_type: "product",
+            posts_per_page: -1
+        });
+    }
+};
+</script>
+
+<template>
+    <div>
+        <div>Imagine a navbar</div>
+        <div>{{ products }}</div>
+        <div>Imagine a footer</div>
+    </div>
+</template>
