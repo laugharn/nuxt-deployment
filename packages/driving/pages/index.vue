76c630589a3953b125c5ae25cc1925d21db28efc
Dan Laugharn <laugharn@gmail.com>
2018-08-30 08:16:07 -0500

    🤞 Adds home page

--- packages/driving/pages/index.vue
+++ packages/driving/pages/index.vue
@@ -1,3 +1,21 @@
+<script>
+import DiyImage from "@core/components/diy/image.vue";
+
+export default {
+    components: {
+        DiyImage
+    },
+
+    head() {
+        return {
+            title: "Be Aceable"
+        };
+    }
+};
+</script>
+
 <template>
-    <div>Hello</div>
+    <div class="bg-brand d-flex" style="position:absolute;bottom:0;left:0;right:0;top:0">
+        <DiyImage src="/uploads/sites/3/2018/06/be-aceable.png" wrapper-extension-classes="m-auto" />
+    </div>
 </template>
